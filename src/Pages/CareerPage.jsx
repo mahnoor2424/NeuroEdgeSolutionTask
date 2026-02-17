dd1e97f56191d609e2ff0a84e7d13fed4d181d93
mahnoor2424 <maharukh646@gmail.com>
2026-02-17 10:47:32 -0800

    hehe

--- src/Pages/CareerPage.jsx
+++ src/Pages/CareerPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Career from '../Components/Career'
 import HiringJourney from '../Components/HiringJourney'
 import FilterDashboard from '../Components/FilterDashboard'
-import Random from '../Components/random'
+import Random from '../Components/Random'
 
 const CareerPage = () => {
   return (
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
     <>
      
       <Hero />
-      <Works />
+      <Works/>
       <Services />
       <TestimonialsSection />
       <CTA />
